0d526ffc30a063b49351e0ac6e17e2e99b324ea7
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-03-21 09:35:54 +1100

    Enable users to log in and sign up new authors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,175 @@
+import { useState, useEffect } from 'react'
+import { jwtDecode } from 'jwt-decode';
+import styled from "styled-components";
+import SignIn from './components/SignIn';
+// import MainView from './components/MainView';
 
+// Styled Components
+
+const StyledHeader = styled.header`
+  padding: 20px;
+
+  & h1 {
+    font-family: 'Big Shoulders Stencil';
+    font-size: 2.5rem;
+    font-weight: 900;
+    letter-spacing: 25px;
+    width: 100%;
+  }
+
+
+  & i {
+    display: none;
+  }
+
+  @media (min-width: 450px) {
+    & i {
+      display: inline-block;
+    }
+  }
+`
+
+const StyledFooter = styled.footer`
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  padding: 12px;
+`
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [signInStatus, setSignInStatus] = useState('logged out');
+  const [viewingPost, setViewingPost] = useState(null)
+  const [posts, setPosts] = useState([])
+  const [comments, setComments] = useState([]);
+
+  function updateViewingPost(post) {
+    setViewingPost(post)
+  }
+
+  function setLocalStorage(key, value) {
+    if (typeof value === "object") {
+      localStorage.setItem(key, JSON.stringify(value));
+    } else {
+      localStorage.setItem(key, value);
+    }
+  }
+
+  function logOut() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setUsername("");
+    setSignInStatus('logged out')
+  }
+
+  function logIn(usernameData) {
+    setUsername(usernameData);
+    setSignInStatus('logged in')
+  }
+
+  function viewSignUp() {
+    setSignInStatus('signing up')
+  }
+
+  async function deleteComment(commentId) {
+    const token = localStorage.getItem("token");
+    await fetch(`http://localhost:3000/users/${username}/comments/${commentId}`, {
+      method: "DELETE",
+      headers: {
+          "Content-type": "application/json",
+          "Authorization": `Bearer ${token}`
+      }
+    }).catch(error => console.error(error));
+    getComments();
+  }
+
+  async function getComments() {
+    fetch('http://localhost:3000/comments', {mode: 'cors'})
+    .then((response) => {
+      if (response.status >= 400) {
+        const error = new Error("Server Error");
+        error.status = response.status;
+        throw error;
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setComments(data.comments)
+    })
+    .catch((error) => {
+      console.error(error.message)
+      setComments([])
+    })
+  }
+
+  function isTokenExpired(token) {
+    try {
+      const decoded = jwtDecode(token);
+      const currentTime = Date.now() / 1000;
+      return decoded.exp < currentTime;
+    } catch(error) {
+      console.error(error);
+      return true;
+    }
+  }
+
+  useEffect(() => {
+    const usernameData = localStorage.getItem("user");
+    const tokenData = localStorage.getItem("token");
+
+    if (tokenData && isTokenExpired(tokenData)) {
+      logOut();
+    } else {
+      if (tokenData) {
+        setSignInStatus('logged in');
+      }
+      if (usernameData) {
+        const parsedUsernameData = JSON.parse(usernameData);
+        setUsername(parsedUsernameData.username)
+      }
+    }
+
+    fetch('http://localhost:3000/posts', {mode: 'cors'})
+    .then((response) => {
+      if (response.status >= 400) {
+        const error = new Error("Server Error");
+        error.status = response.status;
+        throw error;
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setPosts(data.posts)
+    })
+    .catch(error => console.error(error.message))
+
+
+    fetch('http://localhost:3000/comments', {mode: 'cors'})
+    .then((response) => {
+      if (response.status >= 400) {
+        const error = new Error("Server Error");
+        error.status = response.status;
+        throw error;
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setComments(data.comments)
+    })
+    .catch((error) => {
+      console.error(error.message)
+      setComments([])
+    })
+  }, []);
 
   return (
     <>
-      <h1>Hello World</h1>
+      <StyledHeader>
+        <h1>Co.Blog<i className="fa-solid fa-pencil"></i></h1>
+        <h2>Author Client</h2>
+      </StyledHeader>
+      <hr />
+      <SignIn usernameData={username} setLocalStorage={setLocalStorage} viewSignUp={viewSignUp} signInStatus={signInStatus} logOut={logOut} logIn={logIn} updateViewingPost={updateViewingPost}/>
     </>
   )
 }
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,163 @@
+import React from "react";
+import styled from "styled-components";
+import { useState } from "react"
+
+const StyledSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    padding: 12px;
+    width: 100%;
+    align-items: left;
+    gap: 20px;
+
+    & ul {
+            display: flex;
+            flex-direction: column;
+            gap: 5px;
+    }
+
+    & ul li {
+        display: flex;
+        gap: 12px;
+        justify-content: space-between;
+    }
+
+    & ul button {
+        width: 100%;
+    }
+
+    @media (min-width: 1100px) {
+        flex-direction: row;
+        align-items: center;
+
+        & ul {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            align-items: center;
+        }
+    }
+`
+
+const SignIn = ({signInStatus, usernameData, viewSignUp, setLocalStorage, logOut, logIn, updateViewingPost}) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+    async function handleSubmitLogin(event) {
+        event.preventDefault();
+
+        const response = await fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({ username, password}),
+        });
+
+        if (response.ok) {
+
+            const data = await response.json();
+            setLocalStorage("token", data.token);
+
+            const userResponse = await fetch(`http://localhost:3000/users/${username}`, {
+                method: "GET",
+                headers: {
+                    "Content-type": "application/json",
+                    "Authorization": `Bearer ${data.token}`
+                }
+            });
+
+            const userData = await userResponse.json();
+            setLocalStorage("user", userData.user);
+
+            logIn(userData.user.username);
+        } else {
+            console.error("Login failed");
+        }
+    }
+
+    async function handleSubmitSignUp(event) {
+        event.preventDefault();
+        const isAuthor = 'true';
+
+        const response = await fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({ username, password, email, isAuthor }),
+        });
+
+        if (response.ok) {
+            logOut();
+        } else {
+            console.error("Login failed");
+        }
+    }
+
+    if (signInStatus === 'logged in') {
+        return (
+            <StyledSection>
+                <h2>Hi {usernameData}</h2>
+                <button title="View All Posts" onClick={() => updateViewingPost(null)}><i className="fa-solid fa-eye"></i> All Posts</button>
+                <button onClick={logOut}><i className="fa-solid fa-right-from-bracket"></i> Log Out</button>
+            </StyledSection>
+        );
+    } else if (signInStatus === 'signing up') {
+        return (
+            <StyledSection>
+                <h2>Sign Up</h2>
+                <form onSubmit={handleSubmitSignUp}>
+                    <ul>
+                        <li>
+                            <label htmlFor="username">Username </label> 
+                            <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                        </li>
+                        <li>
+                            <label htmlFor="password">Password </label>
+                            <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                        </li>
+                        <li>
+                            <label htmlFor="email">Email </label>
+                            <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        </li>
+                        <li>
+                            <button type="submit">Submit</button>
+                        </li>
+                        <li>
+                            <button onClick={logOut}>Back</button>
+                        </li>
+                    </ul>
+                </form>
+            </StyledSection>
+        )
+    } else {
+        return (
+            <StyledSection>
+                <h2>Sign In</h2>
+                <form onSubmit={handleSubmitLogin}>
+                    <ul>
+                        <li>
+                            <label htmlFor="username">Username </label> 
+                            <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                        </li>
+                        <li>
+                            <label htmlFor="password">Password </label>
+                            <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                        </li>
+                        <li>
+                            <button type="submit">Submit</button>
+                        </li>
+                        <li>
+                            <button onClick={viewSignUp}>Sign Up</button>
+                        </li>
+                    </ul>
+                </form>
+                <button title="View All Posts" onClick={() => updateViewingPost(null)}><i className="fa-solid fa-eye"></i> All Posts</button>
+            </StyledSection>
+        );
+    }
+}
+
+export default SignIn;
