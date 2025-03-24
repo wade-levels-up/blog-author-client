175954f3eab6e3cfca0e9c77e4cc771f20f59c20
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-03-24 18:34:34 +1100

    Fix typo in import of BlogListItem component

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import BlogListItem from "./blogListItem";
+import BlogListItem from "./BlogListItem";
 import Comment from "./Comment";
 
 const StyledMain = styled.main`
